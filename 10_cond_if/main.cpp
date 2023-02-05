c3c5d9029dd45542626d50f0ae424b9089534b4e
Felipe Grijalva <felipegrijalva@gmail.com>
2023-02-05 16:51:57 -0500

    cond if

---
+++ 10_cond_if/main.cpp
@@ -0,0 +1,37 @@
+
+
+/* Incluir librerias */
+#include <iostream> // para entrada y  salida de datos
+using namespace std;
+
+/* Definir constantes */
+/* Los veremos mas adelante  */
+
+/* Definir tipos de datos */
+/* Los veremos mas adelante  */
+
+/* Definir prototipos de nuestras funciones */
+/* Los veremos mas adelante  */
+
+/* Programa principal */
+int main()
+{
+    /* Declarar e inicializar variables locales */
+    int a;
+    
+    /* Sentencias */
+    
+    cout << "Introduzca un numero entero positivo: ";
+    cin >> a;
+    if (a%2 == 0)
+        cout << "El numero " << a << " es un numero par\n";
+
+    return 0;
+}
+
+/* Definir funciones */
+/* Los veremos mas adelante  */
+
+
+
+
