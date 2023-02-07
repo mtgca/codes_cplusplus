1f8ff8a49b61cc0e40519beef568507db975ceeb
Felipe Grijalva <felipegrijalva@gmail.com>
2023-02-07 09:59:33 -0500

    if2

---
+++ 11_cond_if_2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int dia, mes, anyo, fecha;
+    
+    cout << "Introduzca una fecha en formato aaaammdd: ";
+    cin >> fecha;
+    if (fecha > 0)
+    {
+        dia = fecha%100;
+        fecha = fecha/100;
+        mes = fecha%100;
+        anyo = fecha/100;
+        cout << "La fecha es: "
+        << dia << "/"
+        << mes << "/"
+        << anyo << endl;
+    }
+
+    return 0;
+}
+
