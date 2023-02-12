ab4ea9d7a34de2215b6a5ab7bcf0c1cd0aba1ce8
Felipe Grijalva <felipegrijalva@gmail.com>
2023-02-12 17:53:32 -0500

    casting

--- 09_casting_conversion/main.cpp
+++ 09_casting_conversion/main.cpp
@@ -44,7 +44,8 @@ int main()
     cout << left << setw(40) << setfill('*') << "De caracter a entero " << endl;
     num = car; /* Conversion implicita que asigna 49 a la variable num */
     cout << "casting implicito de car: " << num << endl;
-    num = (int) car; /* Conversion explicita que asigna 49 a la variable num */
+    num = (int) car; /* Conversion explicita que asigna 49 a la variable num (c-like notation) */
+    num = int(car); /* Lo mismo pero con "functional notation"*/
     cout << "casting explicito de car: " << num << endl;
     cout << setw(40) << setfill('*') << "" << endl << endl;
     
@@ -52,7 +53,7 @@ int main()
     cout << left << setw(40) << setfill('*') << "De entero a caracter " << endl;
     letra = num1; /* Conversion implicita: asigna el byte de menor peso en num1 (01000001, decimal 65, caracter 'A') a la variable letra.*/
     cout << "casting (implicito) de num1: " << letra << endl;
-    letra = (char) num2; /* Conversion explicita: asigna el byte de menor peso en num2 (01101000, decimal 104, caracter 'h') a la variable letra*/
+    letra = char(num2); /* Conversion explicita: asigna el byte de menor peso en num2 (01101000, decimal 104, caracter 'h') a la variable letra*/
     cout << "casting (explicito) de num2: " << letra << endl;
     cout << setw(40) << setfill('*') << "" << endl << endl;
     
@@ -61,7 +62,7 @@ int main()
     num3 = num4; /* Conversion implicita que asigna
                   11111101 11101000 (decimal -536) a la variable num3 */
     cout << "casting (implicito) de num4: " << num3 << endl;
-    num3 = (short) num4; /* Conversion explicita que asigna -536 a num3*/
+    num3 = short(num4); /* Conversion explicita que asigna -536 a num3*/
     cout << "casting (implicito) de num4: " << num3 << endl;
     cout << setw(40) << setfill('*') << "" << endl << endl;
     
@@ -69,11 +70,11 @@ int main()
     cout << left << setw(40) << setfill('*') << "De entero a real" << endl;
     nreal = n1; /* Conversion implicita que asigna 3.0 a la variable nreal */
     cout << "casting (implicito) de n1: " << nreal << endl;
-    nreal = (float) n1; /* Conversion explicita */
+    nreal = float(n1); /* Conversion explicita */
     cout << "casting (explicito) de n1: " << nreal << endl;
     nreal = n2; /* Conversion implicita: asigna 2147483648.0 a nreal */
     cout << "casting (implicito) de n2: " << setprecision(15) << nreal << endl;
-    nreal = (float) n2; /* Conversion explicita */
+    nreal = float(n2); /* Conversion explicita */
     cout << "casting (explicito) de n2: " << nreal << endl;
     cout << setw(40) << setfill('*') << "" << endl << endl;
     
@@ -81,22 +82,22 @@ int main()
     cout << left << setw(40) << setfill('*') << "De real a entero" << endl;
     num5 = nreal2; /* Conversion implicita que asigna 16 a la variable num */
     cout << "casting (implicito) de nreal2: " << num5 << endl;
-    num5 = (short int) nreal2; /* Conversion explicita */
+    num5 = (short) nreal2; /* Conversion explicita */
+    //num5 = short(nreal2); /* Conversion explicita, functional notation */
     cout << "casting (explicito) de nreal2: " << num5 << endl;
     cout << setw(40) << setfill('*') << "" << endl << endl;
     
     cout << left << setw(40) << setfill('*') << "De real a real de menor tamano" << endl;
     n3 = n4; /* Conversion implicita: asigna 32433.320312 a la variable n1 */
     cout << "casting (implicito) de n4: " << n3 << endl;
-    n3 = (float) n4; /* Conversion explicita */
+    n3 = float(n4); /* Conversion explicita */
     cout << "casting (explicito) de n4: " << n3 << endl;
     cout << setw(40) << setfill('*') << "" << endl << endl;
     
     cout << left << setw(40) << setfill('*') << "Casting con varios operandos" << endl;
     res = numerador/den + 3.7; /* Asigna el valor 4 a la variable res */
     cout << "casting (implicito) de expresion: " << res << endl;
-    
-    res = (float) numerador/den + 3.7; /* Asigna el valor 4 a la variable res */
+    res = float(numerador)/den + 3.7; /* Asigna el valor 4 a la variable res */
     cout << "casting (explicito) de expresion: " << res << endl;
     cout << setw(40) << setfill('*') << "" << endl << endl;
     
