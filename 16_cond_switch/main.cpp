b7eea5952aa9916385dfc12797dedf0477e9f0b5
Felipe Grijalva <felipegrijalva@gmail.com>
2023-02-28 19:31:13 -0500

    .

--- 16_cond_switch/main.cpp
+++ 16_cond_switch/main.cpp
@@ -5,21 +5,21 @@ int main()
 int num_mes;
    cout << "Ingrese el número del mes : ";
    cin >> num_mes;
-   switch(num_mes)
+   switch(num_mes) //1 2 3 4 ... 12
    {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
+        case 1: //ene
+        case 3: //mar
+        case 5: //may
+        case 7: //jul
+        case 8: //ago
+        case 10:// oct
+        case 12: // dic
                 cout << "El mes tiene 31 días. \n";
                 break;
         case 2:
                 cout << "Febrero tiene 28 días o 29 días si el anio es bisiesto. \n";
                 break;
-        case 4:
+        case 4: //abri
         case 6:
         case 9:
         case 11:
