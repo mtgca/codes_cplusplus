17a4c17c200dcbc1e40a110edcc6f45b390fc073
Felipe Grijalva <felipegrijalva@gmail.com>
2023-02-28 19:37:13 -0500

    wh

---
+++ 25_for_while_equivalencia/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    //version con for
+    
+    int i;
+    for(i=12; i>=1; i=i-1)
+        cout << i << endl;
+    
+    //version con while
+    
+    /*
+    int i;
+    i = 12;
+    while (i>=1)
+    {
+        cout << i << endl;
+        i = i-1;
+    }
+    */
+    
+    return 0;
+}
