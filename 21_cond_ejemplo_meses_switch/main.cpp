e118bf235b0dda5e1654f0a30b986025e39c9d69
Felipe Grijalva <felipegrijalva@gmail.com>
2023-03-01 20:41:39 -0500

    s

--- 21_cond_ejemplo_meses_switch/main.cpp
+++ 21_cond_ejemplo_meses_switch/main.cpp
@@ -58,3 +58,47 @@ int main()
     return 0;
 }
 
+// solucion optimizada: Autor: Martín Montero, Mateo Vivanco
+/*
+#include <iostream>
+using namespace std;
+int main (){
+    int dia, mes, error{0};
+    string estacion{"invierno"};
+    cout << "\nIngrese el dia y el mes: ";
+    cin >> dia >> mes;
+    if(dia < 1 || dia > 31) error = 1;
+    else
+        switch(mes){
+            case 1:
+            case 2:
+            case 3:
+                if (dia < 21 || mes < 3) break;
+            case 4:
+            case 5:
+            case 6:
+                if (dia < 21 || mes < 6) {
+                    estacion = "primavera";
+                    break;
+                }
+            case 7:
+            case 8:
+            case 9:
+                if (dia < 21 || mes < 9) {
+                    estacion = "verano";
+                    break;
+                }
+            case 10:
+            case 11:
+            case 12:
+                if (dia < 21 || mes < 12) estacion = "otono";
+                break;
+            default:
+                error = 1;
+        }
+    if (error) cout << "\nError!! Fecha incorrecta";
+    else cout << "\nEl " << dia << "/" << mes << " es " << estacion;
+    return 0;
+}
+*/
+
