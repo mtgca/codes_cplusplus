716ba3718b1aba18e39e1237a40e56b6fbcb4563
Felipe Grijalva <felipegrijalva@gmail.com>
2023-03-09 10:55:59 -0500

    33

---
+++ 33_while_good_fail/main.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    double result = 0;
+    double input;
+    cout << "Ingrese los números que desea sumar. ";
+    cout << "Para parar, presione cualquier tecla" << endl;
+    while (true)
+    {
+
+        cin >> input;
+        if (cin.good())
+        {
+            result += input;
+        }
+        if (cin.fail())
+        {
+            cout << "Sum = " << result << endl;
+            break;
+            
+        }
+    }
+    
+    return 0;
+
+}
