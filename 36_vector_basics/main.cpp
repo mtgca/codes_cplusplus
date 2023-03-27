c44491e672f24c3e7cda58d07fb355b783e6bdcb
Felipe Grijalva <felipegrijalva@gmail.com>
2023-03-27 12:06:03 -0500

    .

--- 36_vector_basics/main.cpp
+++ 36_vector_basics/main.cpp
@@ -11,12 +11,14 @@ int main() {
     
     // array con inicializacion
     int ages[] = {71, 42, 37, 5, 18};
-    cout << ages[0] << endl;
-    cout << ages[4] << endl;
+    cout << "ages[0] = " << ages[0] << endl;
+    cout << "ages[5] = " << ages[5] << endl; //genera solo warning, no es error
+    cout << "ages[-1] = " << ages[-1] << endl; //genera solo warning, no es error
+    cout << "ages = " << ages << endl; // imprime la dirección de memoria del primer elemento
     
     // array sin inicializacion
     int ages2[5];
-    cout << "Valor indefinido: " << ages2[4] << endl;
+    cout << "Valor indefinido: " << ages2[4] << endl; //alguien mas debe inicializar (cin)
     
     // array de strings
     string names[] = {"Alan", "Bob", "Carol", "David", "Ellen"};
@@ -27,6 +29,11 @@ int main() {
     double decimals[] = {2.3, 4};
     int integers[2] = {1, 2}; //previene narrowing !
     cout << bools[0] << endl;
+    
+    // array de chars
+    char cadena[5] = "ABCD";//almacena 4 letras + caracter NULL '\0'
+    cout << "Tam en bytes de cadena: " << sizeof(cadena) << endl;
+    cout << "cadena[] = " << cadena[2] << endl;
     
     // modificando un array
     int grades[] = {85, 95, 48, 100, 92};
@@ -43,6 +50,7 @@ int main() {
     // numero de elementos de un array
     unsigned int num_elementos = sizeof(friends) / sizeof(friends[0]);
     cout << "Num. elementos: " << num_elementos << endl;
+    //cout << "Tam en bytes del array ages[0]: " << ages[0].size() << endl; // no existe en ints
     
     // iterar en un array
     for (int i = 0; i < num_elementos; i++) {
