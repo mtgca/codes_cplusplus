07c0a095d51931504aa480a46919de05e392235c
Felipe Grijalva <felipegrijalva@gmail.com>
2023-03-29 10:48:59 -0500

    .

--- 38_array_struct/main.cpp
+++ 38_array_struct/main.cpp
@@ -12,7 +12,14 @@ int main() {
     personas[0] = {"Alice", 30, 1.75};
     personas[1] = {"Bob", 25, 1.80};
     personas[2] = {"Charlie", 35, 1.65};
-
+    
+    /* ingresando con cin
+    for (int k = 0; k < TAMANIO_ARREGLO; k++) {
+        cin >> personas[k].nombre;
+        cin >> personas[k].edad;
+        cin >> personas[k].estatura;
+    }
+     */
 
     for (int i = 0; i < TAMANIO_ARREGLO; i++) {
         cout << personas[i].nombre << " tiene " << personas[i].edad << " años y mide "
