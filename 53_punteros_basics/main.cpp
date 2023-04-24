8686f3575951305015afc09ac5d448dd30e75fe1
Felipe Grijalva <felipegrijalva@gmail.com>
2023-04-24 16:17:07 -0500

    k

---
+++ 53_punteros_basics/main.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    
+    int num= 9;
+    int *pnum;
+    pnum = &num; // La variable pcar es inicializada con la direccion de la variable num
+    cout << "Son direcciones: pnum = " << pnum << " y &num = " << (&num) << endl; // pnum y &num son direcciones de memoria.
+    cout << "Tamanio de pnum en bytes= " << sizeof(pnum) << endl;
+    cout << "Son enteros: *pnum = " << *pnum << " y num = " << num << endl; // *pnum y num son enteros
+    
+    
+    // Para imprimir direcciones de char es necesario hacer un casting a (void*)
+    char car = 'c';
+    char *pcar;
+    pcar = &car; // La variable pcar es inicializada con la direccion de la variable car
+    cout << "Son direcciones: pcar = " << (void*) pcar << " y &car = " << (void*)(&car) << endl; // pcar y &car son direcciones de memoria.
+    cout << "Son caracteres: *pcar = " << *pcar << " y car = " << car << endl; // *pcar y car son caracteres
+    cout << "Tamanio de pcar en bytes= " << sizeof(pcar) << endl;
+    
+    
+    
+    int i=5,j=3;
+    int *p;
+    p = &i; /* La variable p es inicializada con la direccion de
+    la variable i. p apunta a i */
+    j = *p; /* Se asigna a la variable j el valor al cual apunta
+    p, es decir, se asigna el valor de i. Estas dos
+    sentencias son equivalentes a j = i; */
+    
+    return 0;
+}
