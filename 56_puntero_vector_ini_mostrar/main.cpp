9841d698aff89d2b4fc4a91493f31aa7207de26d
Felipe Grijalva <felipegrijalva@gmail.com>
2023-04-25 17:41:42 -0500

    k

--- 56_puntero_vector_ini_mostrar/main.cpp
+++ 56_puntero_vector_ini_mostrar/main.cpp
@@ -3,13 +3,10 @@ using namespace std;
 
 #define MAX 3
 
-void mostrar_vector(int vec[]);
+void mostrar_vector(int *vec);
 void inic_vector(int vec[]);
 
-
-
-int main()
-{
+int main(){
     int v[MAX];
     inic_vector(v);
     mostrar_vector(v);
@@ -17,16 +14,13 @@ int main()
 }
 
 /* Funcion que muestra por pantalla los MAX elementos de vec */
-void mostrar_vector(int vec[])
-{
+void mostrar_vector(int *vec){
     for(int i=0; i<MAX; i++)
         cout << vec[i] << " ";
 }
 
-/* Funcion que lee desde el teclado MAX elementos enteros
-y los guarda en vec */
-void inic_vector(int vec[])
-{
+/* Funcion que lee desde el teclado MAX elementos enteros y los guarda en vec */
+void inic_vector(int vec[]){ // o void inic_vector(int *vec), o tambien void inic_vector(int vec[])
     cout << "Introduzca " << MAX << " elementos enteros (separados por un espacio): ";
     for(int i=0;i<MAX;i++)
         cin >> vec[i];
