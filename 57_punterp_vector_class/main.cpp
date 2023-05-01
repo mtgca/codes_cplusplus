d280d157ba63aec3ece77a3f7907d802b01c69c4
Felipe Grijalva <felipegrijalva@gmail.com>
2023-04-30 20:24:42 -0500

    lll

--- 57_punterp_vector_class/main.cpp
+++ 57_punterp_vector_class/main.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 #define MAX 3
 
-void mostrar_vector(vector<int> vec); // pasar vector class por valor
+void mostrar_vector(vector<int> *vec); // pasar vector class por puntero
 void inic_vector(vector<int> &vec); // pasar vector class por referencia
 
 
 int main(){
     vector<int> v(MAX);
     inic_vector(v);
-    mostrar_vector(v);
+    mostrar_vector(&v);
     return 0;
 }
 
 /* Funcion que muestra por pantalla los MAX elementos de vec */
-void mostrar_vector(vector<int> vec){
+void mostrar_vector(vector<int> *vec){
     for(int i=0; i<MAX; i++)
-        cout << vec[i] << " ";
+        cout << vec->at(i) << " "; // no se puede usar vec[i] porque vec es una clase, no un array.
 }
 
 /* Funcion que lee desde el teclado MAX elementos enteros y los guarda en vec */
